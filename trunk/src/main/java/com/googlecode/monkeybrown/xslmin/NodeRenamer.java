ddce03210c08ad4754521dc3fee8e000eee6b9e9
ricksbrown73@gmail.com <ricksbrown73@gmail.com@b5752836-7e32-1f50-ba39-79a50736f7a9>
2011-12-18 04:54:22 +0000

    Previous commit: 	- Added Ant task This commit: 	- Fixed typo (which had no adverse impact on anything except minor performance hit) 	- Time taken for minification is now printed at end

--- trunk/src/main/java/com/googlecode/monkeybrown/xslmin/NodeRenamer.java
+++ trunk/src/main/java/com/googlecode/monkeybrown/xslmin/NodeRenamer.java
@@ -14,67 +14,67 @@ public abstract class NodeRenamer
 	public abstract String getClashTestXpath();
 	public abstract int renameNode(Node node, String oldName, String newName) throws XPathExpressionException;
 	public abstract boolean canRemoveNode(Node node);
-	
+
 	public void rename() throws XPathExpressionException
 	{
 		NameGenerator nameGenerator = new NameGenerator();
 		List<Node> toRemove = new ArrayList<Node>();
-		
+
 		//find all named nodes that need renaming
 		NodeList nodes = this.getNodesToRename();
-		
-	    //loop over each named node
-	    for(int i = 0, len = nodes.getLength(); i < len; i++) 
-	    {
-	    	Node next = nodes.item(i);
-	    	Node name = next.getAttributes().getNamedItem("name");
-	    	String oldName = name.getNodeValue();
-	    	String newName = nameGenerator.getNextName(next, this.getClashTestXpath());
+
+		//loop over each named node
+		for(int i = 0, len = nodes.getLength(); i < len; i++)
+		{
+			Node next = nodes.item(i);
+			Node name = next.getAttributes().getNamedItem("name");
+			String oldName = name.getNodeValue();
+			String newName = nameGenerator.getNextName(next, this.getClashTestXpath());
 			if(newName.length() < oldName.length())//only rename if it makes the name shorter
 			{
 				name.setNodeValue(newName);
-	    		int referers = this.renameNode(next, oldName, newName);
-	    		if(referers < 1 && this.canRemoveNode(next))
-	    		{
-	    			System.out.println("Deleting unused node " + oldName);
-	    			toRemove.add(next);
-	    		}
+				int referers = this.renameNode(next, oldName, newName);
+				if(referers < 1 && this.canRemoveNode(next))
+				{
+					System.out.println("Deleting unused node " + oldName);
+					toRemove.add(next);
+				}
 			}
-	    }
-	    NodeRenamer.removeNodes(toRemove);
+		}
+		NodeRenamer.removeNodes(toRemove);
 	}
-	
+
 	/**
 	 * Removes each node in the list from its DOM tree
 	 * @param toRemove The list of nodes to remove
 	 */
 	public static void removeNodes(NodeList toRemove)
 	{
-		for(int i = 0, len = toRemove.getLength(); i < len; i++) 
-	    {
-	    	Node next = toRemove.item(i);
-	    	Node parent = next.getParentNode();
-	    	if(parent != null)
-	    	{
-	    		parent.removeChild(next);
-	    	}
-	    }
+		for(int i = 0, len = toRemove.getLength(); i < len; i++)
+		{
+			Node next = toRemove.item(i);
+			Node parent = next.getParentNode();
+			if(parent != null)
+			{
+				parent.removeChild(next);
+			}
+		}
 	}
-	
+
 	/**
 	 * Removes each node in the list from its DOM tree
 	 * @param toRemove The list of nodes to remove
 	 */
 	public static void removeNodes(List<Node> toRemove)
 	{
-		for(int i = 0, len = toRemove.size(); i < len; i++) 
-	    {
-	    	Node next = toRemove.get(i);
-	    	Node parent = next.getParentNode();
-	    	if(parent != null)
-	    	{
-	    		parent.removeChild(next);
-	    	}
-	    }
+		for(int i = 0, len = toRemove.size(); i < len; i++)
+		{
+			Node next = toRemove.get(i);
+			Node parent = next.getParentNode();
+			if(parent != null)
+			{
+				parent.removeChild(next);
+			}
+		}
 	}
 }
